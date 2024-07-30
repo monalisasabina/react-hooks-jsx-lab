d5ee712b70b08aed5ed00cebef523d8bcafb76a1
Monalisa Onyango <+>
2024-07-31 00:54:43 +0300

    jsx

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div>
+            <h2>About Me</h2>
+            <p>Wow I made this! I don't mind sharing</p>
+            <img src={image} alt="I made this"
+
+            />
+        </div>;
 }
 
 export default About;
