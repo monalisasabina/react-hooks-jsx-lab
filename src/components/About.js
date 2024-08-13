e496b28735f8da58da69cd69fb6b01f3f8ab62c7
Monalisa Onyango <+>
2024-08-13 22:10:23 +0300

    conter checking

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>
+  return <div id="about"> 
             <h2>About Me</h2>
             <p>Wow I made this! I don't mind sharing</p>
             <img src={image} alt="I made this"
